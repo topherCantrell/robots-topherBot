99795e64d0e3534ad72f91964a07d418c1472dd6
Chris Cantrell <topherCantrell@gmail.com>
2022-10-25 12:27:41 -0500

    multi plots

--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 #fname = 'data/G_Base_50_forward.json'
 #fname = 'data/H_Base_100_forward.json'
 #fname = 'data/I_Base_50_backward.json'
-fname = 'data/J_Base_100_backward.json'
+#fname = 'data/J_Base_100_backward.json'
 #fname = 'data/K_Base_L100forward_R100backward.json'
 #fname = 'data/L_Base_no_power_north.json'
 
@@ -31,13 +31,21 @@ def load_data(fname):
 
     return xdata,ydata,zdata
 
-xdata,ydata,zdata = load_data(fname)
+x1,y1,z1 = load_data('data/F_Base_no_power.json')
+x2,y2,z2 = load_data('data/H_Base_100_forward.json')
+for i in range(len(z2)):
+    z2[i] -= 10
+x3,y3,z3 = load_data('data/J_Base_100_backward.json')
+for i in range(len(z3)):
+    z3[i] += 10
 
 fig = plt.figure()
 ax = plt.axes(projection='3d',xlabel='X',ylabel='Y',zlabel='Z',title='Compass Spin')
 
 ax.scatter3D(0,0,0,c='blue',s=100)
 
-ax.scatter3D(xdata,ydata,zdata,c='green',s=5)
+ax.scatter3D(x1,y1,z1,c='green',s=5)
+ax.scatter3D(x2,y2,z2,c='red',s=5)
+ax.scatter3D(x3,y3,z3,c='yellow',s=5)
 
 plt.show()
